6584dd54c75596a873a1f4a477f03f0c703deda0
DChan0319 <chaosstriker0319@gmail.com>
2018-04-11 19:52:53 -0700

    Pushing onto my own branch

--- client/index.jsx
+++ client/index.jsx
@@ -8,6 +8,6 @@ import App from './components/App';
 
 const store = createStore(allReducers);
 
-ReactDOM.render( <Provider store={store}>
+ReactDOM.render(<Provider store={store}>
    <App />
 </Provider>, document.getElementById("app"));
