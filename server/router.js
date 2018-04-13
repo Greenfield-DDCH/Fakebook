4fa4675c8d0d68816aa4b7b722208e12262b15c8
Hack Reactor Student <student@hackreactor.com>
2018-04-12 17:35:27 -0700

    :)

--- server/router.js
+++ server/router.js
@@ -10,14 +10,6 @@ const router = Router();
 
 router.route('/user/setstatus')
     .post(profileController.EditStatus)
-import express from 'express'
-import { Router } from 'express';
-import Profile from './controller.js'
-
-const router = Router();
-
-router.route('/user/setstatus')
-    .post(Profile.Status)
 
 
 export default router;
