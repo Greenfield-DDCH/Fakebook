f9c58f0ffce04e121af99a57da4236bf3ab2b095
DChan0319 <chaosstriker0319@gmail.com>
2018-04-13 19:31:00 -0700

    Rearrange file structure

--- server/models/model.js
+++ server/models/model.js
@@ -1,4 +1,4 @@
-import dbConnection from './db';
+import dbConnection from '../db';
 
 export function getPosts(profileId,callback){
   dbConnection.query(`select u.username, p.post, p.type from users u inner join posts p on (u.id = p.user_id) where p.profile_id = ${profileId}`, function(err, result){
