3c9b78ce1bd87e61235ba70403a93399937de448
DChan0319 <chaosstriker0319@gmail.com>
2018-04-19 15:20:42 -0700

    Fixed FriendPage bugs, no persisting posts/comments. TODO: Posts Css.

--- server/models/searchModel.js
+++ server/models/searchModel.js
@@ -1,8 +1,10 @@
 import dbConnection from '../db';
 
 const searchModel = function (username, callback) {
+  // console.log("inside model**************",username);
   dbConnection.query(`SELECT * FROM users WHERE username = "${username}"`, function (err, result, fields) {
     if (err) { throw err; } 
+    // console.log("result of Search",result)
     callback(err, {results: result});
   });    
 }
