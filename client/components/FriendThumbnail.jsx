e3231be710cd9726ed4526bddeeee040d8249a21
DChan0319 <chaosstriker0319@gmail.com>
2018-04-19 14:37:18 -0700

    Fixed FriendList Avatar onClick(). TODO: Posts Css

--- client/components/FriendThumbnail.jsx
+++ client/components/FriendThumbnail.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {Image} from 'semantic-ui-react';
+import axios from 'axios';
 
 import {setCurrentUser} from '../actions/index.js';
 
@@ -10,15 +11,37 @@ class FriendThumbnail extends Component {
     super(props);
   }
 
-  onUsernameClick(usernames, pictures, userId) {
-    var user = {id: userId, username: usernames, password: '', picture: pictures};
-    this.props.setCurrentUser(user);
+  onUsernameClick(username) {
+    var context = this;
+    axios({
+      method: 'get',
+      url: `/api/search/${username}`,
+      headers: { token: sessionStorage.getItem('token') },
+    }).then((response) => {
+      context.props.setCurrentUser(response.data.results[0]);
+      console.log('after the setting of current user', context.props.currentUser);
+      //Search for posts, will probably need to grab friends as well
+      //fetch the CurrentProfile's posts and update them
+      axios({
+        method: 'get',
+        url: `/api/posts/${context.props.currentUser.id}`,
+        headers: { token: sessionStorage.getItem('token') },
+      }).then((res)=>{
+        console.log('successful get', res);
+        context.props.changeCurrentUsersPosts(res.data);
+      }).catch(function(err) {
+        console.log(error);
+      });
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
     this.props.goToProfile();
   }
 
   render() {
     return (
-      <div className="friendPageAvatar" onClick={() => this.onUsernameClick(this.props.username, this.props.picture, this.props.userId)}>
+      <div className="friendPageAvatar" onClick={() => this.onUsernameClick(this.props.username)}>
         <Image size='tiny' src={this.props.picture} avatar />
         <span>{this.props.username}</span>
       </div>
